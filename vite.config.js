823d489e1095d099141f87ea08b52de3795d2a21
YourGitHubUsername <your-email@example.com>
2025-07-28 00:45:16 +0530

    Fix: remove merge conflict in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-<<<<<<< HEAD
-export default defineConfig({
-
-  plugins: [react()],
-  base: './',
-=======
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Weather-App",
->>>>>>> fdda5bb6d2c04b8e31db3754132044ca9f23752e
-})
+});
